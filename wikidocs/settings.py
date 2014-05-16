8c0ed7f8071332ef625430ed49f1de17c7dfe1ed
pahkey <pahkey@gmail.com>
2014-05-16 13:56:02 +0900

    gunicorn

--- wikidocs/settings.py
+++ wikidocs/settings.py
@@ -1,6 +1,7 @@
 # Django settings for wikidocs project.
 import os
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 import djcelery
 djcelery.setup_loader()
@@ -53,24 +54,12 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = False
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = SITE_ROOT+'/../upload'
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
+STATIC_ROOT = BASE_DIR+'/book/static'
+STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR+'/upload'
 MEDIA_URL = '/images/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
