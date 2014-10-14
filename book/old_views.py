36a4f27cb0e76a2f1619ff8dd8e5a4f79f6f07fe
EungYong Park <pahkey@gmail.com>
2014-10-15 01:03:49 +0900

    api error patch

--- book/old_views.py
+++ book/old_views.py
@@ -6,26 +6,31 @@ import urllib2
 
 
 def api_page(request, no):
-    data = urllib.urlencode({})
-    header = {}
-    req = urllib2.Request("http://old.wikidocs.net/api/page/%s" % no, data, header)  # POST request doesn't not work
-    return HttpResponse(urllib2.urlopen(req).read())
+
+    # data = urllib.urlencode({})
+    # header = {}
+    # req = urllib2.Request("http://old.wikidocs.net/api/page/%s" % no, data, header)  # POST request doesn't not work
+    # return HttpResponse(urllib2.urlopen(req).read())
+
+    return HttpResponse("Sorry, no longer supported.")
 
 
 def api_toc(request):
-    pageid = request.POST["pageid"]
-    data = urllib.urlencode({'pageid': pageid})
-    header = {}
-    req = urllib2.Request("http://old.wikidocs.net/api/toc", data, header)  # POST request doesn't not work
-    return HttpResponse(urllib2.urlopen(req).read())
+    # pageid = request.POST["pageid"]
+    # data = urllib.urlencode({'pageid': pageid})
+    # header = {}
+    # req = urllib2.Request("http://old.wikidocs.net/api/toc", data, header)  # POST request doesn't not work
+    # return HttpResponse(urllib2.urlopen(req).read())
+    return HttpResponse("Sorry, no longer supported.")
 
 
 def api_comment(request):
-    pageid = request.POST["pageid"]
-    author = request.POST["author"]
-    comment = request.POST["comment"]
-
-    data = urllib.urlencode({'pageid': pageid, 'author':author, 'comment':comment})
-    header = {}
-    req = urllib2.Request("http://old.wikidocs.net/api/comment", data, header)  # POST request doesn't not work
-    return HttpResponse(urllib2.urlopen(req).read())
+    # pageid = request.POST["pageid"]
+    # author = request.POST["author"]
+    # comment = request.POST["comment"]
+    #
+    # data = urllib.urlencode({'pageid': pageid, 'author':author, 'comment':comment})
+    # header = {}
+    # req = urllib2.Request("http://old.wikidocs.net/api/comment", data, header)  # POST request doesn't not work
+    # return HttpResponse(urllib2.urlopen(req).read())
+    return HttpResponse("Sorry, no longer supported.")
