bd352ebba06f9299bc0b5610475bf3c4c9ff9baa
pahkey <pahkey@gmail.com>
2014-12-30 13:18:58 +0900

    add pub_date to rss

--- book/feeds.py
+++ book/feeds.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.syndication.views import Feed
 from django.shortcuts import get_object_or_404
-from django.utils.encoding import force_unicode
-from django.utils.safestring import mark_safe
-import markdown
 
 from book.models import Book
 
@@ -24,12 +21,5 @@ class BookFeed(Feed):
     def items(self, obj):
         return obj.recent_pages(limit=30)
 
-    def item_title(self, item):
-        return item.subject
-
-    def item_description(self, item):
-        extensions = ["tables", "toc", "footnotes", "fenced_code"]
-        return mark_safe(markdown.markdown(force_unicode(item.content),
-                                           extensions,
-                                           safe_mode=True,
-                                           enable_attributes=False))
+    def item_pubdate(self, item):
+        return item.modify_time
